b3f897921db6ac73db4d9bf95dea6f4ecb943999
Dev-020 <82759695+Dev-020@users.noreply.github.com>
2025-11-16 01:05:33 +0800

    Migrated misc_scripts into its own repository
    Moved them to another repository to avoid polluting the main files

--- misc_scripts/Gather/fish/__init__.py
+++
@@ -1,6 +0,0 @@
-# my_package/__init__.py (dynamic __all__)
-from pathlib import Path
-
-__all__ = [
-    module.stem for module in Path(__file__).parent.glob("*.py") if module.name != "__init__.py"
-]
